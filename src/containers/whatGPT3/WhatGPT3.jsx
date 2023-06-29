4ef4e63c0f81caa43c9b8b3a20e9a95728817e52
lakshankd <dominiclakshank@gmail.com>
2023-06-29 23:43:10 +0530

    Adding brand section.

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./whatgpt3.css";
+import Feature from "../../components/feature/Feature";
 
 const WhatGPT3 = () => {
-  return <div>WhatGPT3</div>;
+  return (
+    <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
+      WhatGPT3
+    </div>
+  );
 };
 
 export default WhatGPT3;
